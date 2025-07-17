67f33dd41383f254afc04405c1cdd7d548f14a3e
Nicolas AMATI <skkyz3r.dev@gmail.com>
2025-07-17 23:21:07 +0200

    fixed cards

--- components/card.vue
+++ components/card.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="inset-0 flex flex-col items-center text-white font-sans">
+  <div class="inset-0 flex flex-col items-center text-white font-sans h-full">
     <section
-      class="bg-(--ui-primary)/20 border-2 border-(--ui-primary) p-8 rounded-2xl shadow-lg w-full max-w-xl"
+      class="flex flex-col justify-between bg-(--ui-primary)/20 border-2 border-(--ui-primary) p-8 rounded-2xl shadow-lg w-full max-w-xl h-full"
     >
-      <img
-        v-if="props.imageUrl"
-        :src="props.imageUrl"
-        alt="Project Image"
-        class="w-full h-48 object-cover rounded-lg mb-4"
-      />
-      <h2 class="mb-4 text-3xl font-semibold">{{ props.title }}</h2>
-      <p class="text-lg text-gray-300">{{ props.description }}</p>
-      <UButton :to="projectUrl" class="mt-6" color="primary">
-        View Project
+      <div class="flex-grow">
+        <img
+          v-if="props.imageUrl"
+          :src="props.imageUrl"
+          alt="Project Image"
+          class="w-full h-48 object-cover rounded-lg mb-4"
+        />
+        <h2 class="mb-4 text-3xl font-semibold min-h-[3.5rem]">
+          {{ props.title }}
+        </h2>
+        <p class="text-lg text-gray-300">{{ props.description }}</p>
+      </div>
+      <UButton :to="projectUrl" class="mt-6 w-full" color="primary">
+        <div class="w-full text-center">View Project</div>
       </UButton>
     </section>
   </div>
